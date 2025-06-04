97b9f66fe582d451702e47d74cb8d70bc8041116
unknown <dongkyu9303@gmail.com>
2025-06-04 16:06:50 +0900

    [feat] terraform backend.tf edit

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket        = "tf-backend-state-dklee"
-    key           = "dongkyu/aws-codepipeline-tf/env/dev/terraform.tfstate"
+    key           = "env/dev/terraform.tfstate"
     region        = "ap-northeast-2"
     use_lockfile  = true
     encrypt       = true
